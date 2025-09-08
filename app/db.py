3ad1f78ce36545bf63e5de75a6748455e2edcb69
lpagotto <pagotto.leonardo@gmail.com>
2025-09-08 13:23:12 -0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,24 +1,8 @@
 # app/db.py
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 from .settings import settings
 
-class Base(DeclarativeBase):
-    pass
-
-engine = create_engine(
-    settings.DATABASE_URL,
-    pool_pre_ping=True,
-    pool_size=5,
-    max_overflow=10,
-    future=True,
-)
-
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
