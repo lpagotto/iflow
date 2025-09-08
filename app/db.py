687cf9c6488fab8de6f7b8fef16573e54ea4ce84
lpagotto <pagotto.leonardo@gmail.com>
2025-09-08 15:40:35 -0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,6 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .settings import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, future=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
 Base = declarative_base()
